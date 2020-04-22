b725705a991b3618382d700f80b5f567c05009ca
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-04-22 16:15:49 -0500

    add the socio economic values

---
+++ socio_economic_values.rb
@@ -0,0 +1,11 @@
+VulnerablePopulation.find_or_create_by(description: "Retirados de la fuerza publica")
+VulnerablePopulation.find_or_create_by(description: "Grupos étnicos")
+VulnerablePopulation.find_or_create_by(description: "Población victima")
+VulnerablePopulation.find_or_create_by(description: "Personas LGBTIQ+")
+VulnerablePopulation.find_or_create_by(description: "Población migrante")
+VulnerablePopulation.find_or_create_by(description: "Personas en Proceso de reintegración y reincorporación")
+VulnerablePopulation.find_or_create_by(description: "Personas en condiciones de pobreza")
+
+SocioEconomicLevel.find_or_create_by(description: "Bajo-bajo")
+SocioEconomicLevel.find_or_create_by(description: "Bajo")
+SocioEconomicLevel.find_or_create_by(description: "Medio-bajo")
